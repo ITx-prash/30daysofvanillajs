cb042ce73cbb94ef8328beed8a0320cc1bf84f1b
Prashant Adhikari <prashantadhikari440@gmail.com>
2025-09-21 01:44:18 +0545

    Pr 24: Sticky Nav project

--- javascript30-journey/Day-24/script.js
+++ javascript30-journey/Day-24/script.js
@@ -1,1 +1,12 @@
-// Your JavaScript code goes here
+const nav = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", fixNav);
